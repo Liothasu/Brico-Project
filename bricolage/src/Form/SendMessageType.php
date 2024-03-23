bd5f8c74d26a69a77d3b61739062c575054790be
Liothasu <liothasu.dodo@hotmail.com>
2024-03-23 02:28:56 +0100

    Add user role verification to send

--- bricolage/src/Form/SendMessageType.php
+++ bricolage/src/Form/SendMessageType.php
@@ -6,6 +6,7 @@ use App\Entity\Message;
 use App\Entity\User;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -14,35 +15,42 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SendMessageType extends AbstractType
 {
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('recipient', EntityType::class, [
-            'class' => User::class,
-            'choice_label' => 'username',
-            'attr' => [
-                'class' => 'form-control'
-            ],
-            'query_builder' => function (EntityRepository $er) {
-                return $er->createQueryBuilder('u')
-                    ->where('u.roles LIKE :roleUser')
-                    ->orWhere('u.roles LIKE :roleHandyman')
-                    ->andWhere('u.roles NOT LIKE :roleAdmin')
-                    ->setParameter('roleUser', '%"ROLE_USER"%')
-                    ->setParameter('roleHandyman', '%"ROLE_HANDYMAN"%')
-                    ->setParameter('roleAdmin', '%"ROLE_ADMIN"%');
-            }
-        ])
-        ->add('title', TextType::class, [
-            'attr' => [
-                'class' => 'form-control'
-            ]
-        ])
-        ->add('content', TextareaType::class, [
-            'attr' => [
-                'class' => 'form-control'
-            ]
-        ]);
+            ->add('recipient', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'username',
+                'attr' => [
+                    'class' => 'form-control'
+                ],
+                'query_builder' => function (EntityRepository $er) {
+                    $currentUser = $this->security->getUser();
+
+                    return $er->createQueryBuilder('u')
+                        ->where('u.roles NOT LIKE :roleAdmin')
+                        ->andWhere('u != :currentUser')
+                        ->setParameter('roleAdmin', '%"ROLE_ADMIN"%')
+                        ->setParameter('currentUser', $currentUser);
+                }
+            ])
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('content', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
